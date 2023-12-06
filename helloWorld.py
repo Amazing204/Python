480a1661c195295ecc0aa05d8fc25ea59c096e27
Amazing204 <tarunmulchandani204@gmail.com>
2023-12-06 19:14:21 +0530

    just explaining sahil

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,5 @@
 print("hello Tarun,Its your First Python code It should be hello world ")
 print("run hogya na bhai ??")
+print("sahil ko git hub bta rha hoon kaise kaam krta hain through commands")
  
 
