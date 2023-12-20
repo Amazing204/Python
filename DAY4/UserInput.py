25fdc68bf86fae022ea396ff3316751a67faecce
Amazing204 <tarunmulchandani204@gmail.com>
2023-12-20 23:48:33 +0530

    updated

---
+++ DAY4/UserInput.py
@@ -0,0 +1,8 @@
+#In Python, input() is a built-in function that allows a program to receive user input. 
+#It reads a line from the user, converts it to a string, and returns that string.
+
+naamBato = input("apna naam likh le bhai : ")
+phoneNo = int(input("muje number bhi chaiye : "))
+pointWala = float(input("ab muje koi point wala number do : "))
+
+ 
